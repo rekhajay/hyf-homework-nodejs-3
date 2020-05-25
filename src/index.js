1d6851bffbd6e4a51cc247955cc567506b3f637a
rekhajay <48165295+rekhajay@users.noreply.github.com>
2020-05-24 23:09:09 -0700

    Create an endpoint to GET to /user/:id

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+app.get('/users/:id', (req, res) => {
+ 
+});
+ 
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
